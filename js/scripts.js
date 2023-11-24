a8773e8b30a7d2ddb1e37c75375a8d9054982471
Sown-isme <144699529+sownisme@users.noreply.github.com>
2023-11-24 17:13:59 +0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -188,3 +188,75 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 // Thanh Phat 2k3 ends
+
+// S O N 2k20 start
+
+// Start Slider
+
+var slideIndex = 0;
+showDivs(slideIndex);
+
+carousel();
+
+function plusDiv(n) {
+  showDivs(slideIndex += n);
+}
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("tc-slide");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.display = "block";
+    setTimeout(carousel, 3000);
+  }
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("tc-slide");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
+
+// End Slider
+
+// Start search-box
+
+$(window).scroll(function(){
+  var sticky = $('#search-box'),
+      scroll = $(window).scrollTop();
+  var height = $('.tc-slide').height();
+
+  if (scroll >= height) sticky.addClass('sticky');
+  else sticky.removeClass('sticky');
+});
+
+// End search-box
+
+// Start Responsive of Navigation
+
+function myFunction() {
+  var x = document.getElementById("navil");
+  if (x.className === "navi-left") {
+    x.className += " responsive";
+  } else {
+    x.className = "navi-left";
+  }
+  var y = document.getElementById("navir");
+  if (y.className === "navi-right") {
+    y.className += " responsive";
+  } else {
+    y.className = "navi-right";
+  }
+}
+
+// End Responsive of Navigation
+
+// S O N 2k20 end
